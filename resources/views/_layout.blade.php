6605b12c74efbc87f138b06a8275679ae7809853
이예찬 <blackmagic2@naver.com>
2017-08-03 10:50:13 +0900

    mainpage

---
+++ resources/views/_layout.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DIMS Laboratory</title>
+
+        <!-- Fonts -->
+        <link href="//fonts.googleapis.com/earlyaccess/notosanskr.css" rel="stylesheet" type="text/css">
+        <link href="/css/site.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="topbar">
+            <div class="container">
+                <img class="logo" src="/img/logo.png"/>
+                <ul class="menu">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">People</a></li>
+                    <li><a href="#">Research</a></li>
+                    <li><a href="#">Publications</a></li>
+                    <li><a href="#">Seminar</a></li>
+                    <li><a href="#">Course</a></li>
+                    <li><a href="#">Alumni</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            @yield('content')
+        </div>
+        <footer>
+            Adress : Office 501B, Woojung Information Building, Korea University, 145 Anam-ro, Seongbuk-gu, Seoul, Republic of Korea 02841(136-713)  Tel : +82-2-3290-3566<br>
+            Copyright © 2017, By Data Mining & Information Systems Laboratory, Department of Information and Communications, Korea University, All Rights Reserved.
+        </footer>
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,76 @@
+@extends('_layout')
+
+@section('content')
+    <div class="page-home">
+        <div class="hero">
+            <div class="container">
+                <h1>Welcome to DMIS Laboratory</h1>
+                <p>
+                    Our research interests lie in big data mining which aims to find models,<br>
+                    algorithms, and systems for any kinds of data analysis with applications on<br>
+                    knowledge discovery, learning, and anomaly detection.
+                </p>
+                <h2>Specifically, we focus on the following research topics:</h2>
+                <ul class="topics">
+                    <li>
+                        <img src="/img/web.png" alt="Web data mining">
+                    </li>
+                    <li>
+                        <img src="/img/bio.png" alt="Bio data mining">
+                    </li>
+                    <li>
+                        <img src="/img/stock.png" alt="Stock market data mining">
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="news">
+            <div class="container">
+                <h1>What's New about DMIS Lab</h1>
+                <div class="news-container">
+                    <div class="news-section">
+                        <label class="news-category">DMIS in Media</label>
+                        <ul class="news-list">
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암 text text 치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 text 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                        </ul>
+                    </div>
+                    <div class="news-section">
+                        <label class="news-category">Accepted Paper</label>
+                        <ul class="news-list">
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암 text text 치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 text 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                            <li><a href="#"><strong>[Nov. 2016]</strong> 고려대학교 강재우 교수 연구팀, 암치료에 새 희망 쏘다.</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hire">
+            <div class="container">
+                <h1>HIRING - ALWAYS OPEN</h1>
+                <ul class="hire-list">
+                    <li>
+                        <img src="/img/icon1.png" alt="Undergraduate students">
+                        <div class="desc">Undergraduate Students</div>
+                    </li>
+                    <li>
+                        <img src="/img/icon2.png" alt="Graduate students">
+                        <div class="desc">Graduate Students</div>
+                    </li>
+                    <li>
+                        <img src="/img/icon3.png" alt="Post-doctoral">
+                        <div class="desc">Post-Doctoral</div>
+                    </li>
+                </ul>
+                <label class="feel-free">Feel free to ask us at any time!</label>
+                <a href="#" class="goto-contact">Go to Contact</a>
+            </div>
+        </div>
+    </div>
+@endsection
